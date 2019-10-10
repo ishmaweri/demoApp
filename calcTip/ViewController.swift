b396b65f53213aed2c27dc687655b9608f5f88fd
ishmaweri <51898633+ishmaweri@users.noreply.github.com>
2019-10-09 23:36:08 -0400

    Initial Commit

---
+++ calcTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calcTip
+//
+//  Created by Ismail Mussed on 10/9/19.
+//  Copyright © 2019 Ismail Mussed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
